4256bb03d5e0d293d368f233a9cfd23b8e1c9f9d
Sohee KWON <soheekwon@naver.com>
2021-11-21 07:34:56 +0900

    fix: json-server api 수정

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Question from "../components/result/Question";
 import AnswerBoxWrap from "../components/result/AnswerBoxWrap";
 import NextButton from "../components/result/NextButton";
@@ -6,25 +6,19 @@ import styled from "styled-components";
 import { getAnswers } from "../libs/api";
 
 const Result = () => {
+  const [answers, setAnswers] = useState(null);
+
   useEffect(() => {
     (async () => {
       const data = await getAnswers();
-      console.log(data);
+      setAnswers(data.data.data);
     })();
   }, []);
 
-  const answers = [
-    { name: "person1", answer: "발리에서 스쿠버 다이빙을 하고 싶어요." },
-    { name: "person2", answer: "hello" },
-    { name: "person3", answer: "how are you" },
-    { name: "person4", answer: "hihihi" },
-    { name: "person5", answer: "helloooo" },
-  ];
-
   return (
     <StyledResult>
-      <Question text={"hello"} />
-      <AnswerBoxWrap answers={answers} />
+      <Question text={"당신의 MBTI는?"} />
+      {answers && <AnswerBoxWrap answers={answers} />}
       <NextButton />
     </StyledResult>
   );
