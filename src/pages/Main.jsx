6d372c9905816fe78d71d9afb307064ff63f71b6
n0eyes <doll4586633@naver.com>
2021-11-21 01:48:52 +0900

    initial commit

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { Routes, Route } from "react-router";
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      main
+      <Routes>
+        <Route path="/:3" element={<div>hihi</div>} />
+        <Route path="/:3/result" element={<div>result</div>} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Main;
